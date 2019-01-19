407fc659f7f307de3d0a0e340a9a506835bef7f0
chulanovskyi <skyrocker2013@gmail.com>
2019-01-19 19:55:44 +0200

    add select effect

--- lecture_2/dnd/src/components/Tile.js
+++ lecture_2/dnd/src/components/Tile.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import cn from 'classnames';
 import 'styles/tile.css';
 
-const Tile = React.memo((props) => (
-  <div className="tile-container flex-center">
-    {props.text}
+const Tile = React.memo(({id, isActive, onClick}) => (
+  <div
+    role="button"
+    tabIndex={0}
+    className={cn("tile-container flex-center", {active: isActive})}
+    onClick={onClick}
+  >
+    {id}
   </div>
 ));
 
