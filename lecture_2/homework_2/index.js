17ed4eaf1817a1ed73535d346a393d03e355c6d3
chulanovskyi <skyrocker2013@gmail.com>
2019-01-23 01:13:54 +0200

    crap

--- lecture_2/homework_2/index.js
+++ lecture_2/homework_2/index.js
@@ -1,14 +1,71 @@
 (function() {
   const rootNode = document.getElementById('root');
 
+  const createTag = tag => document.createElement(tag);
+  const appendToNode = (node, item) => node.appendChild(item);
+  const applyStyle = node => (v, k) => (node.style[k] = v);
+
+  const createDiv = styles => {
+    const div = createTag('div');
+    R.forEachObjIndexed(applyStyle(div), styles);
+    return div;
+  };
+
+  const createInputWithLabel = (label, inputName) => {
+    const labelNode = createTag('label');
+    const inputNode = createTag('input');
+
+    labelNode.setAttribute('for', inputName);
+    labelNode.style.margin = '5px';
+    labelNode.innerText = label;
+
+    inputNode.setAttribute('name', inputName);
+    inputNode.setAttribute('id', inputName);
+    labelNode.appendChild(inputNode);
+
+    return labelNode;
+  };
+
+  const createButton = (title, onClick) => {
+    const buttonNode = createTag('button');
+    buttonNode.innerText = title;
+    buttonNode.addEventListener('click', onClick);
+    buttonNode.style.width = 'fit-content';
+    return buttonNode;
+  };
+
+  const colInput = createInputWithLabel('New column:', 'col');
+  const rowInput = createInputWithLabel('New row:', 'row');
+  const itemInput = createInputWithLabel('Item id:', 'item');
+  const confirmButton = createButton('Rearrange', () => rearrangeMatrix());
+
+  const controlsContainer = createDiv({
+    display: 'flex',
+    flexDirection: 'column'
+  });
+
+  appendToNode(controlsContainer, colInput);
+  appendToNode(controlsContainer, rowInput);
+  appendToNode(controlsContainer, itemInput);
+  appendToNode(controlsContainer, confirmButton);
+  appendToNode(rootNode, controlsContainer);
+
+  const tableContainer = createDiv();
+
+  const tableNode = createTag('table');
+
+  appendToNode(tableContainer, tableNode);
+  appendToNode(rootNode, tableContainer);
+
   /* matrix helpers */
   const mapIndexed = R.addIndex(R.map);
+  const getNumberRoot = num => Math.pow(num, 1 / 2);
 
   const countMatrixItems = size => size ** 2;
 
   const generateNumbers = count => R.times(R.identity, count);
 
-  const numbersToChunks = (nums, chunkSize) => R.splitEvery(chunkSize, nums);
+  const numbersToChunks = nums => R.splitEvery(getNumberRoot(nums.length), nums);
 
   const numbersToItems = squareMatrix =>
     mapIndexed(
@@ -25,16 +82,18 @@
       squareMatrix
     );
 
-  const createMatrix = size =>
-    R.pipe(
-      countMatrixItems,
-      generateNumbers,
-      nums => numbersToChunks(nums, size),
-      numbersToItems
-    );
-
-  const rearrangeMatrix = (table, newCol, newRow, tile) => null;
+  const rearrangeMatrix = (matrix, newCol, newRow, item) => console.log('ok');
   /* --- */
-  const m = createMatrix(3)(3);
-  console.log(m);
+
+  const fillTable = (table, matrix) => {};
+
+  // public static void main
+  const createSquareMatrix = R.pipe(
+    countMatrixItems,
+    generateNumbers,
+    numbersToChunks,
+    numbersToItems
+  );
+  const squareMatrix = createSquareMatrix(3);
+  // ---
 })();
